8a6c43b587c808df7b4139420f746e2e41ca6ea9
Danitza Bonilla <danitzabonilla@gmail.com>
2020-09-22 10:20:31 -0500

    Updates

---
+++ precache-manifest.ede806747cb8865b2304a8848b8af42f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ce88f5dfe935c7a9ad412e9b2c03be",
+    "url": "/store-danitza-bonilla/index.html"
+  },
+  {
+    "revision": "d6977ef0e465dc351d35",
+    "url": "/store-danitza-bonilla/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d6acfe525daca6ec26e",
+    "url": "/store-danitza-bonilla/static/js/2.d6bf7edf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/store-danitza-bonilla/static/js/2.d6bf7edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6977ef0e465dc351d35",
+    "url": "/store-danitza-bonilla/static/js/main.ebc14fc1.chunk.js"
+  },
+  {
+    "revision": "b70e82bec2d6a054cbcf",
+    "url": "/store-danitza-bonilla/static/js/runtime-main.cf6016fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/store-danitza-bonilla/static/media/logo.5d5d9eef.svg"
+  }
+]);
